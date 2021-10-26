849232c935a36457811cda4ce56491b1f0c5ee4b
itsusinn <itsusinn@foxmail.com>
2021-10-26 18:38:59 +0800

    refactor: a bridge of message-client logging

---
+++ src/main/kotlin/org/meowcat/mesagisto/kato/Extension.kt
@@ -0,0 +1,29 @@
+package org.meowcat.mesagisto.kato
+
+import org.meowcat.mesagisto.client.ILogger
+import org.meowcat.mesagisto.client.LogLevel
+import org.meowcat.mesagisto.client.Logger
+
+typealias StdLogger = java.util.logging.Logger
+
+fun Logger.bridgeToBukkit(impl: StdLogger) {
+  level = when (impl.level) {
+//    Level.ALL -> LogLevel.TRACE
+//    Level.FINE -> LogLevel.TRACE
+//    Level.INFO -> LogLevel.TRACE
+//    Level.WARNING -> LogLevel.WARN
+//    Level.SEVERE -> LogLevel.ERROR
+//    Level.OFF -> LogLevel.ERROR
+    else -> { LogLevel.TRACE }
+  }
+  provider = object : ILogger {
+    override fun log(level: LogLevel, msg: String) {
+      when (level) {
+//        LogLevel.TRACE -> impl.fine(msg)
+//        LogLevel.DEBUG -> impl.finer(msg)
+        LogLevel.INFO -> impl.info(msg)
+        else -> impl.info(msg)
+      }
+    }
+  }
+}
