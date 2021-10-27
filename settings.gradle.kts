9a2d7178c4a643995d353b89c322658f5bf49459
itsusinn <itsusinn@foxmail.com>
2021-10-27 21:10:23 +0800

    build: add maven local repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "taboo-message-source"
 pluginManagement {
   repositories {
     mavenLocal()
+    maven("./maven-repo")
     gradlePluginPortal()
     mavenCentral()
     google()
