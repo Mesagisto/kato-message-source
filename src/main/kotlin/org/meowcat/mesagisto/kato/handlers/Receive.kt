c46c19870cc483f8507021102aad0a8267b19831
itsusinn <itsusinn@foxmail.com>
2021-11-15 11:55:13 +0800

    fix: wrong toString

--- src/main/kotlin/org/meowcat/mesagisto/kato/handlers/Receive.kt
+++ src/main/kotlin/org/meowcat/mesagisto/kato/handlers/Receive.kt
@@ -3,6 +3,7 @@ package org.meowcat.mesagisto.kato.handlers
 import arrow.core.Either
 import io.nats.client.impl.NatsMessage
 import org.bukkit.Bukkit
+import org.meowcat.mesagisto.client.Base64
 import org.meowcat.mesagisto.client.data.Message
 import org.meowcat.mesagisto.client.data.MessageType
 import org.meowcat.mesagisto.client.data.Packet
@@ -22,8 +23,10 @@ suspend fun receive(
 fun receiveMessage(
   message: Message,
 ): Result<Unit> = runCatching fn@{
-  val senderName = with(message.profile) { nick ?: username ?: id.toString() }
-  val content = message.chain.find { it is MessageType.Text } ?: return@fn
-  val text = "$senderName: $content"
-  Bukkit.broadcastMessage(text)
+  val senderName = with(message.profile) { nick ?: username ?: Base64.encodeToString(id) }
+  val msgList = message.chain.filterIsInstance<MessageType.Text>()
+  msgList.forEach {
+    val text = "<$senderName> ${it.content}"
+    Bukkit.broadcastMessage(text)
+  }
 }
