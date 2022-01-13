aee36703703ffe27079989c955cfc64b0a4eda65
itsusinn <itsusinn@foxmail.com>
2022-01-13 22:10:08 +0800

    build: 更换打包插件为pkg-tool

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.itsusinn.pkg.pkgIn
+
 group = "org.meowcat"
 version = property("project_version")!!
 plugins {
@@ -5,7 +7,7 @@ plugins {
   kotlin("jvm") version "1.5.21"
   kotlin("plugin.serialization") version "1.5.21"
   id("com.github.johnrengelman.shadow") version "7.1.1"
-  id("io.itsusinn.pkg") version "1.0.0"
+  id("io.itsusinn.pkg") version "1.2.0"
 }
 repositories {
   mavenCentral()
@@ -20,7 +22,6 @@ pkg {
   excludePath("*.md")
   excludePath("DebugProbesKt.bin")
   excludePathStartWith("META-INF/maven")
-  excludePathStartWith("org/slf4j")
   shadowJar {
     minimize()
     mergeServiceFiles()
@@ -41,10 +42,10 @@ tasks.compileKotlin {
 
 dependencies {
   compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.2")
-  implementation("io.nats:jnats:2.13.1")
-  implementation("org.meowcat:mesagisto-client-jvm:1.1.2")
+  pkgIn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
+  pkgIn("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.2")
+  pkgIn("io.nats:jnats:2.13.1")
+  pkgIn("org.meowcat:mesagisto-client-jvm:1.1.2")
   // implementation("org.meowcat:mesagisto-client:1.1.1-n4")
   compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
 }
