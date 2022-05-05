46bf509c90bf558c99a820c79bb0bf79c2cb2b30
itsusinn <itsusinn@foxmail.com>
2022-05-05 23:10:08 +0800

    build: exclude useless files

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   kotlin("jvm") version "1.6.0"
   kotlin("plugin.serialization") version "1.6.0"
   id("com.github.johnrengelman.shadow") version "7.1.1"
-  id("io.itsusinn.pkg") version "1.2.0"
+  id("io.itsusinn.pkg") version "1.2.2"
 }
 repositories {
   mavenCentral()
@@ -19,9 +19,21 @@ repositories {
 }
 pkg {
   excludePath("META-INF/*.kotlin_module")
-  excludePath("*.md")
-  excludePath("DebugProbesKt.bin")
+  excludePathStartWith("META-INF/versions")
+  excludePathStartWith("META-INF/proguard")
   excludePathStartWith("META-INF/maven")
+  excludePathStartWith("org/slf4j")
+  excludePathStartWith("org/jetbrains")
+  excludePathStartWith("org/intellij")
+  excludePath("*.md")
+  excludePath("*.js")
+  excludePath("DebugProbesKt.bin")
+  excludePathStartWith("kotlinx/coroutines/flow")
+  listOf("asn1", "jcajce", "jce", "pqc", "x509", "math", "i18n", "iana", "internal").forEach {
+    excludePathStartWith("org/bouncycastle/$it")
+  }
+  excludePathStartWith("META-INF/maven")
+  listOf("stream", "math")
   shadowJar {
     minimize()
     mergeServiceFiles()
@@ -45,8 +57,8 @@ dependencies {
   pkgIn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
   pkgIn("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.2.2")
   pkgIn("io.nats:jnats:2.14.0")
-  pkgIn("org.meowcat:mesagisto-client-jvm:1.3.0")
-  // implementation("org.meowcat:mesagisto-client:1.1.1-n4")
+  pkgIn("org.meowcat:mesagisto-client-jvm:1.4.0")
+  // pkgIn("org.meowcat:mesagisto-client:1.4.0-build3")
   pkgIn("com.github.jknack:handlebars:4.3.0")
   compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
 }
