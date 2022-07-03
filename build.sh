e838afdbeb7683d30f7706d7a59a3ff74648aec7
Itsusinn <itsusinn@meowcat.org>
2022-07-03 16:12:27 +0800

    ci: remove no kotlin-stdlib option

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 build(){
 	rm -rf packages
 	mkdir -p packages
-	export NO_KT="false"
 	./gradlew clean pkg
 	mv build/pkg/*.jar packages/kato.jar
-	export NO_KT="true"
-	./gradlew clean pkg
-	mv build/pkg/*.jar packages/kato-NOKT.jar
 }
 build
